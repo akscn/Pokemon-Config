492f8d10cd1c43edf099362f83a02a5c3849b54c
akscn <cn.akshat2002@gmail.com>
2026-03-27 23:31:10 +0530

    File config commit

---
+++ config.fish
@@ -0,0 +1,5 @@
+# Add this line inside the `if status is-interactive` block in your config.fish
+# Location: ~/.config/fish/config.fish
+
+# Pokemon-JakooLit
+pokemon-colorscripts --no-title -s -r | fastfetch -c ~/.config/fastfetch/config-pokemon.jsonc --logo-type file-raw --logo-height 10 --logo-width 5 --logo -
